a3d5fb1979649d83919b640e48741ec133223250
mani <manikantha96@gmail.com>
2021-12-27 12:21:26 +0530

    Initial Commit

---
+++ TestDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestDemo
+//
+//  Created by Gudla Manikantha on 27/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestDemo/TestDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestDemoApp.swift
+//  TestDemo
+//
+//  Created by Gudla Manikantha on 27/12/21.
+//
+
+import SwiftUI
+
+@main
+struct TestDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
