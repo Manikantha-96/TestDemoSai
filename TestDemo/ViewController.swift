1a6c44ab4d78c99c66101c7e714080e162c85c8e
phanindhra007 <phanindhra@zuddl.com>
2022-08-07 09:10:26 +0530

    add files

---
+++ TestDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Addpackage
+//
+//  Created by Venkata Phanindhra on 28/07/22.
+//
+
+import UIKit
+//import GiphyUISDK
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        Giphy.configure(apiKey: "your ios sdk key here")
+    }
+
+
+}
+
